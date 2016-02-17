59d5b717325b3e86f0df8ff0f86cb0b46d48a4fe
Yunze <yunze.moe@outlook.com>
2016-02-17 22:24:57 +0800

    add database init script

--- test/create_database.js
+++ test/create_database.js
@@ -1,75 +1,67 @@
-var mysql = require('mysql');
-var adminInfo = require('../dbadmin.json');
+var db = require('../lib/database/connect');
 
-var connection = mysql.createConnection({
-    host: adminInfo.host,
-    user: adminInfo.user,
-    password: adminInfo.password,
-    database: adminInfo.database
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-
-connection.query("drop table if exists person");
-connection.query("drop table if exists event");
-connection.query("drop table if exists friend");
-connection.query("drop table if exists part_in");
-connection.query("drop table if exists decision");
-connection.query("drop table if exists vote");
+db.query('ALTER DATABASE chiba_project DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci');
+db.query("drop table if exists person");
+db.query("drop table if exists event");
+db.query("drop table if exists friend");
+db.query("drop table if exists part_in");
+db.query("drop table if exists launch");
+db.query("drop table if exists decision");
+db.query("drop table if exists vote");
 
 
 // create tables
 // person
-connection.query("CREATE TABLE person (" +
-            "id         int             PRIMARY KEY AUTO_INCREMENT," +
-            "phone      varchar(128)    NOT NULL UNIQUE," +
-            "password   varchar(128)    NOT NULL," +
-            "nickname   varchar(128)    NOT NULL)");
+db.query("CREATE TABLE person (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "phone      varchar(128)    NOT NULL UNIQUE," +
+        "password   varchar(128)    NOT NULL," +
+        "nickname   varchar(128)    NOT NULL)");
 
 // event
-connection.query("CREATE TABLE event (" +
-            "id         int             PRIMARY KEY AUTO_INCREMENT," +
-            "manager    int             NOT NULL," +
-            "title      varchar(128)    NOT NULL," +
-            "time       int," + // 如果年数是1970 则日期待定
-            "time_stat  int," + // 确定时间是不是待定 0 待定 1 确定
-            "location   varchar(128)," +
-            "state      int             NOT NULL)");
+db.query("CREATE TABLE event (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "manager    int             NOT NULL," +
+        "title      varchar(128)    NOT NULL," +
+        "time       bigint," + // 如果年数是1970 则日期待定
+        "time_stat  int," + // 确定时间是不是待定 0 待定 1 确定
+        "location   varchar(128)," +
+        "state      int             NOT NULL)");
 
 // friend
-connection.query("CREATE TABLE friend (" +
-            "id         int             PRIMARY KEY AUTO_INCREMENT," +
-            "usr0_id    int             NOT NULL," +
-            "usr1_id    int             NOT NULL)");
+db.query("CREATE TABLE friend (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "usr0_id    int             NOT NULL," +
+        "usr1_id    int             NOT NULL)");
 
 // partin
-connection.query("CREATE TABLE part_in (" +
-            "id         int             PRIMARY KEY AUTO_INCREMENT," +
-            "event_id   int             NOT NULL," +
-            "usr_id     int             NOT NULL)");
+db.query("CREATE TABLE part_in (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "event_id   int             NOT NULL," +
+        "usr_id     int             NOT NULL)");
+
+// launch
+db.query("CREATE TABLE launch (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "usr_id     int             NOT NULL," +
+        "event_id   int             NOT NULL)");
 
 // decision
-connection.query("CREATE TABLE decision (" +
-            "id         int             PRIMARY KEY AUTO_INCREMENT," +
-            "event_id   int             NOT NULL," +
-            "usr_id     int             NOT NULL," +
-            "type       int             NOT NULL," + // 0 time 1 location 2 add_person
-            "content    varchar(128)    NOT NULL," +
-            "agree      int             DEFAULT 0," +
-            "reject     int             DEFAULT 0)");
+db.query("CREATE TABLE decision (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "event_id   int             NOT NULL," +
+        "usr_id     int             NOT NULL," +
+        "type       int             NOT NULL," + // 0 time 1 location 2 add_person
+        "content    varchar(128)    NOT NULL," +
+        "agree      int             DEFAULT 0," +
+        "reject     int             DEFAULT 0)");
 
 // vote
-connection.query("CREATE TABLE vote (" +
-            "id         int             PRIMARY KEY AUTO_INCREMENT," +
-            "decision_id int            NOT NULL," +
-            "usr_id     int             NOT NULL," +
-            "type       int             NOT NULL)");
+db.query("CREATE TABLE vote (" +
+        "id         int             PRIMARY KEY AUTO_INCREMENT," +
+        "decision_id int            NOT NULL," +
+        "usr_id     int             NOT NULL," +
+        "type       int             NOT NULL)");
 
 // to do: add trigger!
-connection.end();
+db.end();
